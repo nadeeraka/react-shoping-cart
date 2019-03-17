65d85d55abc964fb934cc2b09f141810cac0c775
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-17 14:56:04 +0530

    add content 1

--- src/components/product.js
+++ src/components/product.js
@@ -11,7 +11,10 @@ const Product = props => {
       {props.pro ? (
         <div className="col-9 mx-auto col-6 col-3 my-3">
           <div className="card">
-            <div className="img-container p-5" onClick={console.log("pp")}>
+            <div
+              className="img-container p-5"
+              onClick={() => console.log("pp")}
+            >
               <Link to="/details">
                 <img
                   src={props.pro.img}
@@ -19,6 +22,23 @@ const Product = props => {
                   className="card-img-top"
                 />
               </Link>
+              <button
+                className="cart-btn"
+                disabled={props.pro.inCart ? true : false}
+                onClick={() => console.log("clicked")}
+              >
+                {props.pro.inCart ? (
+                  <p className="mb-0" disabled>
+                    IN CART
+                  </p>
+                ) : (
+                  <i className="fas fa-cart-plus" />
+                )}
+              </button>
+            </div>
+            {/* card footer*/}
+            <div className="card-footer d-flex justify-content-between">
+              <p className="align-self-center mb-0">{props.pro.title}</p>
             </div>
           </div>
         </div>
