8e6dd67517d0ec64c54baadf538282ca8b42efaa
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-17 17:44:28 +0530

    11

--- src/components/product.js
+++ src/components/product.js
@@ -9,36 +9,41 @@ const Product = props => {
   return (
     <>
       {props.pro ? (
-        <div className="col-9 mx-auto col-6 col-3 my-3">
-          <div className="card">
-            <div
-              className="img-container p-5"
-              onClick={() => console.log("pp")}
-            >
-              <Link to="/details">
-                <img
-                  src={props.pro.img}
-                  alt="product img"
-                  className="card-img-top"
-                />
-              </Link>
-              <button
-                className="cart-btn"
-                disabled={props.pro.inCart ? true : false}
-                onClick={() => console.log("clicked")}
+        <div className="raw">
+          <div className="col-9 mx-auto col-6 col-3 my-3">
+            <div className="card">
+              <div
+                className="img-container p-5"
+                onClick={() => console.log("pp")}
               >
-                {props.pro.inCart ? (
-                  <p className="mb-0" disabled>
-                    IN CART
-                  </p>
-                ) : (
-                  <i className="fas fa-cart-plus" />
-                )}
-              </button>
+                <Link to="/details">
+                  <img
+                    src={props.pro.img}
+                    alt="product img"
+                    className="card-img-top"
+                  />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={props.pro.inCart ? true : false}
+                  onClick={() => console.log("clicked")}
+                >
+                  {props.pro.inCart ? (
+                    <p className="mb-0" disabled>
+                      IN CART
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
             </div>
             {/* card footer*/}
             <div className="card-footer d-flex justify-content-between">
               <p className="align-self-center mb-0">{props.pro.title}</p>
+              <h5 className="text-blue font-italic mb-0">
+                <span className="mr-1">$ {props.pro.price}</span>
+              </h5>
             </div>
           </div>
         </div>
